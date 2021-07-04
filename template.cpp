b59e7048baf59bc498f773e84d737da21b4d6ad8
jooncco <jooncco.g@gmail.com>
2021-07-04 19:21:01 +0900

    edit template.cpp

--- template.cpp
+++ template.cpp
@@ -7,6 +7,8 @@ typedef deque<int> di;
 typedef deque<ii> dii;
 typedef vector<int> vi;
 typedef vector<ii> vii;
+typedef priority_queue<int, vi, less<int> > maxHeap;
+typedef priority_queue<int, vi, greater<int> > minHeap;
 
 int t;
 
