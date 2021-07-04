cbe11589d24c8cb820e39b6abd414d9c4742ba42
jooncco <jooncco.g@gmail.com>
2021-07-04 19:29:08 +0900

    remove template.cpp. Its not necessary

--- init.cpp
+++ init.cpp
@@ -10,10 +10,25 @@ int main(int argc, char *args[]) {
 
 	ifstream inFile;
 	inFile.open("template.cpp");
-	string line;
-	while (getline(inFile,line)) {
-		outFile << line << "\n";
-	}
+	outFile << "#include <bits/stdc++.h>\n";
+	outFile << "using namespace std;\n\n";
+	outFile << "#define FAST_IO ios_base::sync_with_stdio(0),cin.tie(0)\n";
+	outFile << "typedef pair<int,int> ii;\n";
+	outFile << "typedef deque<int> di;\n";
+	outFile << "typedef deque<ii> dii;\n";
+	outFile << "typedef vector<int> vi;\n";
+	outFile << "typedef vector<ii> vii;\n";
+	outFile << "typedef priority_queue<int, vi, less<int> > maxHeap;\n";
+	outFile << "typedef priority_queue<int, vi, greater<int> > minHeap;\n\n";
+	outFile << "int t;\n";
+	outFile << "int main() {\n\n";
+	outFile << "\tFAST_IO;\n";
+	outFile << "\tcin >> t;\n";
+	outFile << "\twhile (t--) {\n";
+	outFile << "\t\t\n";
+	outFile << "\t}\n";
+	outFile << "}";
+
 	inFile.close();
 
 	outFile.close();
